cc43b7ad1e7d8c9f79d42477b25e227f5ca0ec92
Ari-Uuganbayar <Ari.Uuganbayar@gmail.com>
2021-07-16 00:10:17 +0800

    logo zassan

--- src/components/Logo/index.js
+++ src/components/Logo/index.js
@@ -1,12 +1,12 @@
 import React from "react";
-import css from './style.module.css'
+import css from "./style.module.css";
 
 import logo from "../../assets/images/logo.jpg";
 
 const index = () => {
   return (
     <div className={css.logo}>
-      <img src={logo} />
+      <img src={logo} alt="" />
     </div>
   );
 };
